0cc19ab7fa60256eaec0a4dc222d252692347d00
TristanEPSI <tristan.gottschak@laposte.net>
2025-04-24 10:23:47 +0200

    feat: add radix and file for auth modal

--- src/components/modals/auth.modal.tsx
+++ src/components/modals/auth.modal.tsx
@@ -1,1 +1,2 @@
 // TODO: Auth Modal that is either register or signIn regarding a useState[isRegistering]
+// State can be changed with a link at the end of the modal for switch between registering or signIn
