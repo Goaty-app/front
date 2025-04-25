84adbf643acb5becd0fb384477075bfd7ba8d50e
TristanEPSI <tristan.gottschak@laposte.net>
2025-04-25 10:50:30 +0200

    feat: add auth context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import "./globals.css";
 import Header from "@/components/layout/header/header";
+import {AuthProvider} from "@/context/authContext";
 
 
 export const metadata: Metadata = {
@@ -14,15 +15,17 @@ export default function RootLayout({ children }: Readonly<{
     return (
         <html lang="fr">
             <body className="bg-layer-1 min-h-screen overflow-y-auto w-screen flex flex-col sm:flex-col-reverse dark-text">
-                <div className="flex-1 relative flex flex-col sm:flex-row my-4 mx-2 gap-4">
-                    <aside className="hidden sm:block sticky top-4 h-full w-[20%] bg-layer-2 rounded-sm p-2">
+                <AuthProvider>
+                    <div className="flex-1 relative flex flex-col sm:flex-row my-4 mx-2 gap-4">
+                        <aside className="hidden sm:block sticky top-4 h-full w-[20%] bg-layer-2 rounded-sm p-2">
                         Navigation Comp
-                    </aside>
-                    <main className="bg-layer-2 flex-1 rounded-sm">
-                        {children}
-                    </main>
-                </div>
-                <Header/>
+                        </aside>
+                        <main className="bg-layer-2 flex-1 rounded-sm">
+                            {children}
+                        </main>
+                    </div>
+                    <Header/>
+                </AuthProvider>
             </body>
         </html>
     );
