258c6896236f87e221a24dfe7d79a5961ae50233
Tristan Gottschalk <123099307+TristanEPSI@users.noreply.github.com>
2025-04-28 10:03:06 +0200

    feat: add auth.modal.tsx and call in header

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,13 +1,15 @@
 'use client';
 import React, { useState, useEffect } from 'react';
-import { CircleUserRound, MenuIcon, MoonIcon, Sun } from "lucide-react";
+import { CircleUserRound, LogOut, MenuIcon, MoonIcon, Sun } from 'lucide-react';
 import './header.style.css';
-import {useAuth} from "@/context/authContext";
+import { useAuth } from '@/context/authContext';
+import Link from 'next/link';
+import AuthModal from "@/components/modals/auth.modal";
 
 const Header: React.FC = () => {
     const [isDarkTheme, setIsDarkTheme] = useState(false);
+    const [isAuthModalOpen, setIsAuthModalOpen] = useState(false); // Etat pour la modal
     const { isAuthenticated } = useAuth();
-
 
     useEffect(() => {
         const isDark = document.body.classList.contains('dark-theme');
@@ -27,29 +29,36 @@ const Header: React.FC = () => {
         setIsDarkTheme(willBeDark);
     };
 
+    const handleAuthClick = () => {
+        setIsAuthModalOpen(true);
+    };
+
     return (
-        <header className="bg-layer-2 h-[8%] flex justify-between p-4 items-center">
-            <div className="text-xl font-semibold flex gap-4 items-center">
-                <div> { isAuthenticated ? 'Logo' : 'Graou' }</div>
-                <span className="hidden sm:block">GoatManager</span>
-            </div>
+        <>
+            <header className="bg-layer-2 h-[8%] flex justify-between p-4 items-center">
+                <div className="text-xl font-semibold flex gap-4 items-center">
+                    <Link href="/" className="hidden sm:block">Goaty</Link>
+                </div>
 
-            <div className="flex gap-4 text-xl font-semibold items-center">
-                <div>Alertes</div>
+                <div className="flex gap-4 text-xl font-semibold items-center">
+                    {isAuthenticated && <div>Alertes</div>}
 
-                <button className="btn cursor-pointer" onClick={toggleTheme}>
-                    {isDarkTheme ? <MoonIcon /> : <Sun />}
-                </button>
+                    <button className="btn cursor-pointer icon" onClick={toggleTheme}>
+                        {isDarkTheme ? <MoonIcon /> : <Sun />}
+                    </button>
 
-                <button className="btn cursor-pointer">
-                    <CircleUserRound />
-                </button>
+                    <button className="btn cursor-pointer icon" onClick={handleAuthClick}>
+                        {isAuthenticated ? <LogOut /> : <CircleUserRound />}
+                    </button>
 
-                <button className="btn cursor-pointer sm:hidden">
-                    <MenuIcon />
-                </button>
-            </div>
-        </header>
+                    <button className="btn cursor-pointer sm:hidden">
+                        <MenuIcon />
+                    </button>
+                </div>
+            </header>
+
+            <AuthModal open={isAuthModalOpen} onOpenChange={setIsAuthModalOpen} />
+        </>
     );
 };
 
