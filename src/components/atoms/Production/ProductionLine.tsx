18c2f707f0fa671ede2625853650396c0c5418f6
Elias El Oudghiri <elias.el-oudghiri@etu.univ-cotedazur.fr>
2025-06-11 09:21:58 +0200

    resolution merge

--- src/components/atoms/Production/ProductionLine.tsx
+++ src/components/atoms/Production/ProductionLine.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import { ProductionInterface } from "@/interface/production.interface";
 import { TableCell, TableRow, Tooltip } from "@mui/material";
 
-const ProductionLine = ({ production, ...props }) => {
+const ProductionLine: React.FC<{
+  production: ProductionInterface;
+  children: React.ReactNode;
+}> = ({ production, children }) => {
   return (
     <TableRow className="bg-row " key={production.id} hover>
       <TableCell>{production.productionType.name}</TableCell>
@@ -10,7 +13,7 @@ const ProductionLine = ({ production, ...props }) => {
         {production.quantity} {production.quantityUnit}
       </TableCell>
       <TableCell>
-        Herd: {production.herd.name} ({production.herd.location})
+        Herd: {production.herd.name} ({production.herd.id})
       </TableCell>
       <TableCell className={""}>
         <Tooltip title={production.notes}>
@@ -20,9 +23,9 @@ const ProductionLine = ({ production, ...props }) => {
         </Tooltip>
       </TableCell>
       <TableCell>
-        Date: {new Date(production.production_date).toLocaleDateString()}
+        Date: {new Date(production.productionDate).toLocaleDateString()}
       </TableCell>
-      <TableCell>{props.children}</TableCell>
+      <TableCell>{children}</TableCell>
     </TableRow>
   );
 };
