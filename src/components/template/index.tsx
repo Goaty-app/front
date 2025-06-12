f8470a2eb7f859a273267cc8833aa1aa0f0c5483
NicolasP <p1926177@forge.univ-lyon1.fr>
2025-06-12 13:31:44 +0200

    :recycle: refacto commit bug

--- src/components/template/index.tsx
+++ src/components/template/index.tsx
@@ -17,7 +17,6 @@ const Template = {
   HeaderTemplate,
   AnimalDetailsTemplate,
   HealthcareTemplate,
-<<<<<<< Updated upstream
   ProductionSearchTemplate,
   LoadingErrorTemplate,
   LoadingTemplate,
