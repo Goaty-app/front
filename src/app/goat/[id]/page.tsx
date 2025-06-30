bcb38d43cff716068e6aebf0618306842ba97974
NicolasP <nicolaspatino77@gmail.com>
2025-06-30 13:39:09 +0200

    :recycle: fix lint

--- src/app/goat/[id]/page.tsx
+++ src/app/goat/[id]/page.tsx
@@ -1,14 +1,32 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { Suspense } from "react";
 import AnimalDetailPage from "@/components/Pages/AnimalDetailPage";
 import { getAnimalById } from "@/service/animal.service";
+import type { Animal } from "@/interface/animal.interface";
 
-export default async function Page() {
+export default function Page() {
   const { id } = useParams();
-  const animalRes = await getAnimalById(id as string);
-  const animal = animalRes.data;
+  const [animal, setAnimal] = useState<Animal | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (id) {
+      getAnimalById(id as string).then((res) => {
+        setAnimal(res.data);
+        setLoading(false);
+      });
+    }
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (!animal) {
+    return <div>Animal introuvable</div>;
+  }
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
